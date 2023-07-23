d06b14759bf9b7e679d8a4ba2b2045fdc92c04e3
avyunusemre <deu.av.yunusemre@gmail.com>
2023-07-23 13:10:19 +0200

    ikinci commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Merhaba, bu benim ilk commitim");
+        System.out.println("Merhaba, bu benim ikinci commitim");
     }
 }
